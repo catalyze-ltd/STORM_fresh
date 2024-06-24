2ae998a676bc515e0964c0d6bc15a0b82eb96c09
willsharrock23 <168423863+willsharrock23@users.noreply.github.com>
2024-06-24 16:49:48 +0100

    Delete islands/Run_Batch_MOEs.tsx

--- islands/Run_Batch_MOEs.tsx
+++
@@ -1,9 +0,0 @@
-function OverviewMap() {
-    return(
-        <div>
-            <iframe src="https://www.catalyzeconsulting.com/" width="100%" height="800px"></iframe>
-        </div>
-    );
-}
-
-export default OverviewMap;
