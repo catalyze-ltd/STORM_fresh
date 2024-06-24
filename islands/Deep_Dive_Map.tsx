cb6d00328c367fa85a6ddd943467cf4b87c8f13f
willsharrock23 <168423863+willsharrock23@users.noreply.github.com>
2024-06-24 16:48:30 +0100

    Delete islands/Deep_Dive_Map.tsx

--- islands/Deep_Dive_Map.tsx
+++
@@ -1,9 +0,0 @@
-function Deep_Dive_Map() {
-    return(
-        <div>
-            <iframe src="https://www.catalyzeconsulting.com/" width="100%" height="800px"></iframe>
-        </div>
-    );
-}
-
-export default Deep_Dive_Map;
